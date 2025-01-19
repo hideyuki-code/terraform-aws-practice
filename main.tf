f898c1db291243afb76407e62f6573cd4b2d97de
zen-hideyuki <h.kitakata@zenplace.co.jp>
2025-01-19 21:03:07 +0900

    feat: 基本VPCインフラの作成
    - CIDRブロック10.0.0.0/16のVPC作成
    - DNSホスト名とDNSサポートを有効化
    - 各設定に説明的なコメントを追加

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# AWSプロバイダーの設定
 terraform {
   required_providers {
     aws = {
@@ -7,6 +8,22 @@ terraform {
   }
 }
 
+# AWSリージョンの指定
 provider "aws" {
   region = "ap-southeast-2"  # シドニーリージョン
+}
+
+# VPCの作成
+resource "aws_vpc" "main" {
+  # VPCのIPv4 CIDRブロックを指定
+  cidr_block           = "10.0.0.0/16"
+  # VPC内のインスタンスにパブリックDNSホスト名を自動的に割り当てる
+  enable_dns_hostnames = true
+  # VPC内でDNS解決を有効にする
+  enable_dns_support   = true
+
+  # タグの設定
+  tags = {
+    Name = "sample-vpc"  # VPCの識別名
+  }
 }
